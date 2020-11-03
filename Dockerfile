3644a5e7f8b3053aa5ccdcd264aa72f7a47b09f2
Florian Georg <florian.alexander.georg@gmail.com>
2020-11-03 20:03:53 +0100

    added swagger via connexion

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,7 @@ COPY requirements.txt .
 RUN pip install -r requirements.txt
 
 COPY . .
-
+COPY swagger . 
 EXPOSE 5000
 
 ENTRYPOINT ["python", "app.py"]
--- app.py
+++ app.py
@@ -1,12 +1,24 @@
-#from os import environ
-from flask import Flask
+#!/usr/bin/env python3
+from os import environ
+import datetime
+import logging
+import connexion
+from connexion import NoContent
 
-app = Flask (__name__)
+def get_summary(text):
+    response = {
+        "summary": "This is a dummy summary" 
+    } 
+    return response
 
+logging.basicConfig(level=logging.INFO)
+app = connexion.App(__name__, specification_dir='swagger/')
+app.add_api('summarize.yaml')
 
-@app.route('/')
-def index():
-    return "Hello World"
+# set the WSGI application callable to allow using uWSGI:
+# uwsgi --http :8080 -w app
+application = app.app
+
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0',port=5000)
