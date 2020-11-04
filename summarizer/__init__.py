843728e5f74c626c4b0c8ba477d4db2da84d5c63
Florian Georg <florian.alexander.georg@gmail.com>
2020-11-04 04:38:37 +0100

    fixed nltk downloads

--- summarizer/__init__.py
+++ summarizer/__init__.py
@@ -1,2 +1,7 @@
+import nltk
+nltk.download("punkt", quiet=True)
+nltk.download("stopwords", quiet=True)
+from nltk.corpus import stopwords
+
 from summarizer.lsa_summarizer import LsaSummarizer
 from summarizer.base_summarizer import BaseSummarizer
